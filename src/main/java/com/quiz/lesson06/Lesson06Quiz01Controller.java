5b71b75efad95b9a62e5c8791ffcf9e02bf7e714
ahyun <kaeruruk@naver.com>
2024-06-27 14:13:04 +0900

    2024년 6월 27일 자습

--- src/main/java/com/quiz/lesson06/Lesson06Quiz01Controller.java
+++ src/main/java/com/quiz/lesson06/Lesson06Quiz01Controller.java
@@ -2,7 +2,10 @@ package com.quiz.lesson06;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/lesson06/quiz01")
 @Controller
@@ -14,8 +17,23 @@ public class Lesson06Quiz01Controller {
 		return "lesson06/addWebSite";
 	}
 	
+	//http://localhost:8080/lesson06/quiz01/list-website-view
 	@GetMapping("/list-website-view")
 	public String listWebSiteView() {
 		return "lesson06/listWebSite";
+	}
+	
+	@ResponseBody
+	@PostMapping("/add-website")
+	public String addWebSite(
+			@RequestParam("title") String title,
+			@RequestParam("link") String link
+			) {
+		
+		// db insert
+		
+		// ajax응답
+		// "성공"
+		return "성공";
 	}
 }
