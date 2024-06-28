0510d5823ae7c1ea780407de74ed6896d9494e96
ahyun <kaeruruk@naver.com>
2024-06-28 15:30:14 +0900

    2024년 6월 28일 자습

--- src/main/java/com/quiz/lesson06/Lesson06Quiz02Controller.java
+++ src/main/java/com/quiz/lesson06/Lesson06Quiz02Controller.java
@@ -2,15 +2,32 @@ package com.quiz.lesson06;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @RequestMapping("/lesson06/quiz02")
 @Controller
 public class Lesson06Quiz02Controller {
 	
-	@GetMapping("add-url-view")
-	public String addUrlView() {
-		return "lesson06/addUrlView";
+	//http://localhost:8080/lesson06/quiz02/add-bookmark-view
+	//즐겨찾기 추가화면
+	@GetMapping("/add-bookmark-view")
+	public String addBookmarkView() {
+		return "lesson06/addBookmark";
 	}
+	
+	//즐겨찾기 리스트화면
+	@GetMapping("/bookmark-list-view")
+	public String BookmarkListView(){
+		return "lesson06/bookmarkList";
+	}
+	
+	@ResponseBody
+	@PostMapping("/delete-bookmark-list")
+	public String deleteBookmarkList() {
+		return "";
+	}
+	
 }
